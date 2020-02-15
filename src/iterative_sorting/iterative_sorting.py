86fc479a74d8295ccfad333af01fcf3e3848c6ea
gebhartn <nicholas.gebhart@gmail.com>
2020-02-14 21:05:26 -0800

    chore: linter

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,37 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+"""
+Iterative sorting algorithms
+"""
+
+
+def selection_sort(arr):
+    """
+    Selection sort implementation
+    """
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
+        # (hint, can do in 3 loc)
 
         # TO-DO: swap
-
-
-
 
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
+    """
+    Bubble sort implementation
+    """
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
+    """
+    Count sort implementation - Stretch
+    """
 
     return arr
+
