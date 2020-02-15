8a3420906834cfe1531400262b42749cd22e2f45
gebhartn <nicholas.gebhart@gmail.com>
2020-02-14 21:07:29 -0800

    chore: linter

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,14 +1,24 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+"""
+Recursive sorting module
+"""
+
+
+def merge(arrA, arrB):
+    """
+    Merge Sort helper function
+    """
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+def merge_sort(arr):
+    """
+    Merge sort implementation
+    """
     # TO-DO
 
     return arr
@@ -16,11 +26,15 @@ def merge_sort( arr ):
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
+    """
+    In-Place Merge sort with O(1) space complexity
+    """
     # TO-DO
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +42,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
