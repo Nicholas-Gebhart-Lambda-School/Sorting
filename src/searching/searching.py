23ba623db6d9d2a2cc0babdfc97e3a4c44415786
gebhartn <nicholas.gebhart@gmail.com>
2020-02-16 21:09:09 -0800

    mvp searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,3 +1,4 @@
+
 """
 Searching module
 """
@@ -9,6 +10,9 @@ def linear_search(arr, target):
     """
     Linear Search implementation
     """
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     # TO-DO: add missing code
 
@@ -16,6 +20,8 @@ def linear_search(arr, target):
 
 
 # STRETCH: write an iterative implementation of Binary Search
+
+
 def binary_search(arr, target):
     """
     Binary Search implementation
@@ -27,19 +33,56 @@ def binary_search(arr, target):
     low = 0
     high = len(arr)-1
 
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        if arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
     # TO-DO: add missing code
 
     return -1  # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search
+
+
 def binary_search_recursive(arr, target, low, high):
     """
     Recursive Binary Search
-    """
 
-    middle = (low+high)//2
+    If high is >= low is our base case to break out of the loop
+
+    If our target matches arr[mid] return mid, huzzah!
+
+    If our target value is less than arr[mid], our target element
+    cannot exist in the high part of the array, so search backwards
+    from the middle element
+
+    Otherwise, search forwards from the middle towards the high index
+    until we've either exhausted the array or found a matching mid
+    """
 
     if len(arr) == 0:
         return -1  # array empty
-        # TO-DO: add missing if/else statements, recursive calls
+
+    if high >= low:
+        mid = low + (high - low) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        if arr[mid] > target:
+            return binary_search_recursive(arr, target, low, mid - 1)
+        return binary_search_recursive(arr, target, mid + 1, high)
+    return -1
+
+
+# arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# length = len(arr)-1
+
+# print(binary_search_recursive(arr, 8, 0, length))
+
