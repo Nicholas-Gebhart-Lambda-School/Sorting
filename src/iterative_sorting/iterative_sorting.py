f0b372cb82515266a64a10ca3f5c970935653389
gebhartn <nicholas.gebhart@gmail.com>
2020-02-18 15:16:35 -0800

    chore: finish iterative

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -20,7 +20,7 @@ def bubble_sort(arr):
     """
     Bubble sort implementation
     """
-    for i in range(0, len(arr)):
+    for i in range(0, len(arr) - 1):
         for j in range(i+1, len(arr)):
             if arr[j] < arr[i]:
                 arr[j], arr[i] = arr[i], arr[j]
